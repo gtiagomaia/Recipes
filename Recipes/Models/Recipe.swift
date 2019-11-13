3454a999e4fd670cf5a0751fae2c2c6b5e0c311d
Tiago Maia <gtiago.maia@gmail.com>
2019-11-13 11:26:21 +0000

    Initial Commit

---
+++ Recipes/Models/Recipe.swift
@@ -0,0 +1,76 @@
+//
+//  Recipe.swift
+//  Recipes
+//
+//  Created by Tiago Maia on 07/11/2019.
+//  Copyright © 2019 Tiago Maia. All rights reserved.
+//
+
+import Foundation
+
+class Recipe: Codable {
+    var id:Int32?
+    var title:String = ""
+    var categoryid:Int32
+    var ingredients:Array<Ingredient> = []
+    var description:String = ""
+    var duration:Int32 = 0 // in minutes
+    
+    
+    init(id:Int32?, title:String, category:Int32, ingredients:[Ingredient], description:String, duration:Int32) {
+        self.id = id
+        self.title = title
+        self.categoryid = category
+        self.ingredients = ingredients
+        self.description = description
+        self.duration = duration
+    }
+}
+
+struct Ingredient: Codable {
+    var name:String = ""
+    var quantity:Int32 = 0
+    var unittype:String = "" //kg., gr., uni.
+    
+    init(name: String, quantity: Int32, unit:String){
+        self.name = name
+        self.quantity = quantity
+        self.unittype = unit
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "name"
+        case quantity = "quantity"
+        case unittype = "unittype"
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        // 2
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+        try container.encode(quantity, forKey: .quantity)
+        try container.encode(unittype, forKey: .unittype)
+    }
+    
+    
+    mutating func decode(to decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+        quantity = Int32(try container.decode(Int.self, forKey: .quantity))
+        unittype = try container.decode(String.self, forKey: .unittype)
+    }
+}
+
+
+
+/**
+ Cada receita é definida por um nome, uma categoria (entrada, sopa, carne, peixe, sobremesa, ...),
+ tempo médio de realização, uma lista de ingredientes e texto descritivo dos passos de realização.
+ As categorias de receitas deverão poder ser editadas,
+ permitindo a inclusão de novas categorias,
+ edição dos seus nomes e remoção das mesmas.
+ Só poderão ser eliminadas as categorias que não tenham receitas associadas.
+ Cada ingrediente é definido pelo nome, quantidade (valor decimal)
+ e respectiva unidade (kg., gr., uni., colheres sopa, colheres de chá, ...).
+ As unidades poderão ser armazenadas através de um campo de texto.
+ */
